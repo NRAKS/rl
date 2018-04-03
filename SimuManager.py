0dd5f03d5f117f2dfc3c17e04114b374ae717c9d
NoriS <noriaki.sonota@gmail.com>
2018-04-03 17:50:51 +0900

    行動決定方策とタスクとシミュレーションマネージャー

---
+++ SimuManager.py
@@ -0,0 +1,106 @@
+import Policy
+from Task import TreeBandit
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+class SimulationManager():
+    def __init__(self, n_agent, Layer, simulationtimes, episodetimes, task):
+        self.N = n_agent
+        self.AgentList = None
+        self.task = task
+        self.simulationtimes = simulationtimes
+        self.episodetimes = episodetimes
+        self.R_share_recode = np.zeros((episodetimes))
+
+    def addRS(self, R, N_act, N_state, Alpha, Gamma, TAlpha, TGamma, N_Epi, N_Simu):
+        self.AgentList = [Policy.RS(R, N_act, N_state, Alpha, Gamma, TAlpha, TGamma, N_Epi, N_Simu) for _ in range(self.N)]
+
+    def GetAgent(self):
+        return self.AgentList
+
+    def CulculationRshare(self):
+        maxQ = np.zeros((self.task.GetNumState()))
+        Qlist = np.array([self.AgentList[n].GetQ() for n in range(len(self.AgentList))])
+        #print("{}".format(Qlist))
+        maxQ = Qlist.max(axis = 2).max(axis = 0)
+        #print("{}".format(maxQ))
+        # for state in range(self.task.GetNumState()):
+        #     for n in range(len(self.AgentList)):
+        # for state in range(self.task.GetNumState()):
+        #     maxQ[state] = max(Qlist[state])
+        return maxQ
+
+    def InitParameter(self):
+        for n in range(len(self.AgentList)):
+            self.AgentList[n].InitParameters()
+
+    def play(self, n_epi):
+        for n in range(len(self.AgentList)):
+            self.AgentList[n].InitState(self.task.GetStartstate())
+        for n in range(len(self.AgentList)):
+            while True:
+                #print("{}".format(n))
+                CurrentState = self.AgentList[n].GetCurrentstate()
+                self.AgentList[n].SerectAction(CurrentState)
+                self.task.EvaluateNextState(CurrentState,self.AgentList[n].GetSerectAction())
+                NextState = self.task.GetNextState()
+                #print("Next:{}".format(NextStateUCB1))
+                Reward = self.task.EvaluateReward(CurrentState,self.AgentList[n].GetSerectAction())
+                #print("reward:{}".format(Reward))
+                self.AgentList[n].Update(self.AgentList[n].GetSerectAction(), CurrentState, NextState, Reward, n_epi)
+                self.AgentList[n].Updatestate(NextState)
+                if NextState >= self.task.GetGoalState():
+                    break
+        
+        R_share = self.CulculationRshare()
+        self.R_share_recode[n_epi] += R_share[0]
+        for n in range(len(self.AgentList)):
+            self.AgentList[n].ShareReference(R_share)
+            self.AgentList[n].CountRsub(n_epi)
+    
+    def GetR_share(self):
+        return self.R_share_recode
+
+    def PlotAverageReward(self):
+    
+        SumReward = np.zeros((self.episodetimes))
+
+        for n in range(len(self.AgentList)):
+            #plt.plot(self.AgentList[n].culculationAve(),label = "RS{}".format(n))
+            SumReward += self.AgentList[n].culculationAve()
+        
+        Ave = SumReward / len(self.AgentList)
+        return Ave
+        # plt.plot(Ave, label="RSpair")
+        # plt.legend()
+        # plt.title("Output")
+        # plt.xlabel("Episode")
+        # plt.ylabel("Reward")
+        # plt.show()
+        # with open('TreeBandit_AveReward.csv', 'a', encoding='UTF-8') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow(Ave)
+
+    def PlotRegret(self, SumRewardIdeal):
+    
+        sumregret = np.zeros((self.episodetimes))
+        for n in range(len(self.AgentList)):
+            #print("sumrewardideal:{}".format(SumRewardIdeal))
+            #print("Agent{} sumreward:{}".format(n, self.AgentList[n].GetSumReward()))
+            regret = SumRewardIdeal - self.AgentList[n].GetSumReward()
+            #plt.plot(regret, label="RS{}".format(n))
+            sumregret += regret
+        RegretAve = sumregret / len(self.AgentList)
+        return RegretAve
+        # plt.plot(sumregret/len(self.AgentList), label = "Average")
+        # # for n in range(len(self.AgentList)):
+        # #     SumReward += self.AgentList[n].GetSumReward()
+        # # plt.plot(SumRewardIdeal - (SumReward / len(self.AgentList)), label = "RSpair")
+        # plt.legend()
+        # plt.title("regret output")
+        # plt.xlabel("Episode")
+        # plt.ylabel("regret")
+        # plt.show()
+
+        
---
+++ Task.py
@@ -0,0 +1,162 @@
+"""
+python3
+実験用タスクまとめクラス
+基本的にやることは、
+    環境設定に必要な情報を受け取り、初期設定
+    行動と現状態を受け取り、次の状態を決定
+    報酬を決定
+    次状態などを返す
+"""
+import Policy
+import random
+import numpy as np
+
+
+class Environment(object):
+    def __init__(self):
+        self.Currentstate = 0
+        self.NextState = 0
+        self.reward = 0
+        self.N_State = 0
+        self.startstate = 0
+
+    def GetNextState(self):
+        return self.NextState
+
+    def GetReward(self):
+        return self.reward
+
+    def GetNumState(self):
+        return self.N_State
+
+    def GetStartstate(self):
+        return self.startstate
+        
+class Bandit():
+    def Make_Bandit(self, N_Act):
+        Pro = []
+        for i in range(N_Act):
+            Pro.append(i / 10)
+        return Pro
+
+    def Make_linerBandit(self, N_Pro):
+        Pro = []
+        for i in range(N_Pro):
+            for n in range(N_Pro - i):
+                Pro.append(i / 10)
+        return Pro
+
+class TreeBandit(Environment):
+    #初期化,層の厚さを受け取る
+    def __init__(self, layer, SimulationTimes=0, EpisodeTimes=0):
+        super().__init__()
+        self.layer = layer
+        self.Pro = np.asarray(
+            [[0.4, 0.5, 0.6, 0.7], [0.9, 0.8, 0.1, 0.2],
+            [0.7, 0.1, 0.3, 0.6], [0.6, 0.4, 0.1, 0.3],
+            [0.5, 0.2, 0.3, 0.1], [0.3, 0.6, 0.5, 0.8]]
+            )
+        #層の数からバンディットの数を計算
+        self.N_Bandit = 0
+        self.N_State = 0
+        for n in range(layer+1):
+            self.N_State += 4 ** n
+            if n < layer:
+                self.N_Bandit +=  4 ** n
+            if n == layer-1:
+                self.Goal = self.N_Bandit
+        
+        self.n_simu = SimulationTimes
+        self.n_epi = EpisodeTimes
+
+        #それぞれのバンディットに確率listからランダムに報酬期待値を当てはめる
+        self.Bandit = np.zeros((self.N_Bandit, 4))
+        for n in range(self.N_Bandit):
+            self.Bandit[n] = random.choice(self.Pro)
+
+    #次の状態判定
+    def EvaluateNextState(self, Currentstate, CurrentAction):
+        self.NextState = 4 * Currentstate + CurrentAction + 1
+    
+    #報酬を返す
+    def EvaluateReward(self, State, CurrentAction):
+        return self.Bandit[[State], [CurrentAction]]
+    
+    def GetGoalState(self):
+        return self.Goal
+    
+    def PrintBandit(self):
+        print("{}".format(self.Bandit))
+
+    #生成されたツリーバンディットの最適報酬を求める
+    def SeekIdeal(self):
+        n_Act = 4
+        n_state = self.GetNumState()
+        EpisodeTimes = self.n_epi
+        SimulationTimes = self.n_simu
+        Agent = Policy.MinTrial(n_Act, n_state, EpisodeTimes, SimulationTimes)
+        for _ in range(4**(self.layer+1)):
+            CurrentState = self.startstate
+            while True:
+                Agent.SerectAction(CurrentState)
+                self.EvaluateNextState(CurrentState, Agent.GetSerectAction())
+                NextState = self.GetNextState()
+                Reward = self.EvaluateReward(CurrentState, Agent.GetSerectAction())
+                Agent.Update(Reward)
+                CurrentState = NextState
+                if CurrentState >= self.GetGoalState():
+                    Agent.InitSumReward()
+                    break
+        
+        return Agent.GetMaxReward()
+
+class EasyMaze(Environment):
+    #簡単なステージを作る
+    def __init__(self, Row, Col, start, goal):
+        super().__init__()
+        self.row = Row
+        self.col = Col
+        self.start = start
+        self.goal = goal
+
+    #座標に変換
+    def CoordToState(self, row, col):
+        return ((row * self.col) + col)
+    #座標からx軸を算出
+    def StateToRow(self, state):
+        return ((int)(state / self.col))
+    #座標からy軸を算出
+    def StateToCol(self, state):
+        return (state % self.col)
+    #次の座標を算出
+    def EvaluateNextState(self, action, state):
+        Upper = 0
+        Lower = 1
+        Left = 2
+        Right = 3
+
+        row = self.StateToRow(state)
+        col = self.StateToCol(state)
+
+        if action == Upper:
+            if (row) > 0:
+                row-=1
+        elif action == Lower:
+            if (row) < (self.row-1):
+                row+=1
+        elif action == Right:
+            if (col)<(self.col-1):
+                col+=1
+        elif action == Left:
+            if (col)>0:
+                col-=1
+                
+        self.NextState = self.CoordToState(row, col)
+
+    #報酬判定
+    def EvaluateReward(self, state):
+        if state == self.goal:
+            return 1
+        else:
+            return 0
+    
